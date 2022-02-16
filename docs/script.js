42879419d3cf63639c4b3e8ce1832e7980e17a5c
cam <furfangsfleas@gmail.com>
2022-02-16 17:12:55 -0600

    So much progress!
    Colors have been updated, animations have been added, the layout has been improved and much more. This should serve as a good foundation for the desired product and demo/WIP page.

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,11 @@
-function init() {
-    var btn = document.querySelectorAll("button.contact-button");
+/* I've never written JS before! Woohoo! */
 
-    btn.forEach(element => element.addEventListener("click", showContent));
+function init() {
+    var buttons = document.querySelectorAll("button.contact-button");
+    buttons.forEach(element => element.addEventListener("click", showContent));
+
+    var projects = document.querySelectorAll("div.content-section")
+    projects.forEach(element => element.addEventListener("click", goToProject))
 }
 
 function showContent(e) {
@@ -14,5 +18,18 @@ function showContent(e) {
     buttonElem.style.visibility = "hidden";
     buttonElem.style.display = "none";
     contentElem.style.visibility = "visible";
-    contentElem.style.display = "flex";
+    contentElem.style.display = "block";
+}
+
+function goToProject(e) {
+    var srcElem = e.target.id
+    console.log(srcElem)
+
+    switch (srcElem) {
+        case "matrix-keyboard-pi":
+            window.open("https://github.com/not-virus/matrix-keyboard-pi", '_blank')
+            break;
+        default:
+            break;
+    }
 }
